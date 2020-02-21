5c3b59fcfd96de70c66a9bde6a0c00cf4ae60745
Aye Myat Thin Aung <ayemyatthinaung@achromex.com>
2020-02-21 16:56:08 +0630

    first commit

---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,19 @@
+//import 'package:flutter/material.dart';
+//import 'package:personal_expense_app/models/transaction.dart';
+//import 'package:personal_expense_app/widgets/new_transaction.dart';
+//
+//class UserTransactions extends StatefulWidget {
+//  @override
+//  _UserTransactionsState createState() => _UserTransactionsState();
+//}
+//
+//class _UserTransactionsState extends State<UserTransactions> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Column(
+//      children: <Widget>[
+//        NewTransaction(_addNewTransaction),
+//      ],
+//    );
+//  }
+//}
